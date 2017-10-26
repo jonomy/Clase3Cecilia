af085c33df4260abbb08093badf00de7fb1de9d2
jonomy <ceciliabelmar12345@gmail.com>
2017-10-26 20:40:08 -0300

    Creacion clase con estructura completa atributo costructor y metodos

---
+++ src/clase3/caballo.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase3;
+
+/*
+ *
+ * @author LAB06
+ */
+public class caballo 
+{
+  String altura;
+  String color;
+  String peso;
+  String talla;
+  String raza;
+  int patas = 4;
+  // metodo constructor
+  
+}
+public caballo(String nuevoNombre)
+{
+raza = nuevoNombre;
+}
+//metodos  de get y set de  la clase
+public relinchar()
+{
+ 
+}
+//Metodo para obtener la raza del caballo
+public String obtenerRaza()
+{
+//Sentencia obligatoria de retorno
+retun raza;
+}
