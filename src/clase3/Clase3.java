37230123eefb8920974bb09366d9eba7eed3c51a
jonomy <ceciliabelmar12345@gmail.com>
2017-10-26 19:18:01 -0300

    clase3 nuevo  proyecto

---
+++ src/clase3/Clase3.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase3;
+
+/******************************************************************************
+ *cecilia belmar
+ * @author LAB06
+ * Fecha: octubre 26 de  201
+ * Progragra: clase3
+ *Responsabilidad: clase principal 
+ *****************************************************************************/
+public class 
+{
+   
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
